fc21a5c4b9811e88e49a6aef9a0e04b2fef053b6
Mechlus <mechlusdev@gmail.com>
2025-03-12 10:49:42 -0400

    Fix MeshBuffer

--- MeshBuffer.cpp
+++ MeshBuffer.cpp
@@ -6,6 +6,7 @@ using namespace scene;
 
 MeshBuffer::MeshBuffer() {
 	buffer = new irr::scene::SMeshBuffer();
+    bbox.reset(vector3df(0,0,0));
 }
 
 MeshBuffer::~MeshBuffer() {
@@ -25,6 +26,13 @@ void MeshBuffer::pushFace(const Vector3D& v1, const Vector3D& v2, const Vector3D
     buffer->Vertices[currentIndex] = S3DVertex(vector3df(v1.x, v1.y, v1.z), vector3df(n1.x, n1.y, n1.z), SColor(c1.w, c1.x, c1.y, c1.z), vector2df(uvw1.x, uvw1.y));
     buffer->Vertices[currentIndex + 1] = S3DVertex(vector3df(v2.x, v2.y, v2.z), vector3df(n2.x, n2.y, n2.z), SColor(c2.w, c2.x, c2.y, c2.z), vector2df(uvw2.x, uvw2.y));
     buffer->Vertices[currentIndex + 2] = S3DVertex(vector3df(v3.x, v3.y, v3.z), vector3df(n3.x, n3.y, n3.z), SColor(c3.w, c3.x, c3.y, c3.z), vector2df(uvw3.x, uvw3.y));
+
+    if (currentIndex == 0)
+        bbox.reset(buffer->Vertices[currentIndex].Pos);
+
+    bbox.addInternalPoint(buffer->Vertices[currentIndex].Pos);
+    bbox.addInternalPoint(buffer->Vertices[currentIndex + 1].Pos);
+    bbox.addInternalPoint(buffer->Vertices[currentIndex + 2].Pos);
 
     buffer->Indices.reallocate(buffer->Indices.size() + 3);
     buffer->Indices.set_used(buffer->Indices.size() + 3);
